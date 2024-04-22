7c8ed1d0c2bbf07a669aed924edece8234f0bb91
Oleksii Honcharenko <mofree@Oleksiis-MacBook-Pro.local>
2024-04-21 22:06:30 -0500

    Working on ASG

---
+++ setproject/asg.tf
@@ -0,0 +1,68 @@
+#Adding autoscaling group in a zone specified in the variables file using an instance group manager as a target
+
+
+provider "google" {
+  project = "your-project-id"
+  region  = "us-central1"
+}
+
+resource "google_compute_autoscaler" "autoscaler" {
+  name   = "my-autoscaler"
+  zone   = "us-central1-f"
+  target = google_compute_instance_group_manager.gmigm.id
+
+  autoscaling_policy {
+    max_replicas    = 3
+    min_replicas    = 1
+    cooldown_period = 60
+  }
+}
+
+resource "google_compute_instance_template" "template" {
+  provider = google
+
+  name           = "my-instance-template"
+  machine_type   = "e2-medium"
+  can_ip_forward = false
+
+  disk {
+    source_image = data.google_compute_image.debian_9.id
+  }
+
+  network_interface {
+    network = google_compute_network.global_vpc.name
+  }
+
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+resource "google_compute_target_pool" "tgpool" {
+  provider = google-beta
+
+  name = "my-target-pool"
+}
+
+resource "google_compute_instance_group_manager" "gmigm" {
+  name = "my-igm"
+  zone = "us-central1-f"
+
+  version {
+    instance_template = google_compute_instance_template.template.id
+    name              = "primary"
+  }
+
+  target_pools       = [google_compute_target_pool.tgpool.id]
+  base_instance_name = "autoscaler-sample"
+}
+
+data "google_compute_image" "debian_9" {
+  family  = "debian-9"
+  project = "debian-cloud"
+}
+
+resource "google_compute_target_pool" "default" {
+  name = "my-target-pool"
+}
---
+++ setproject/provider.tf
@@ -0,0 +1,6 @@
+#Setting provider for ASG
+provider "google-beta" {
+    region = "us-central1"
+    zone   = "us-central-a"
+  
+}
